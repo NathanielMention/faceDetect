8182875e228c9e3faa3ae8953f254eaf28ba10f2
NathanielMention <nathanielmention@gmail.com>
2023-02-20 19:05:25 -0500

    install openvc

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('init')
+import pathlib
+import cv2
+
+# import xml files for faces from cv2
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute(
+) / "data/haarcascade_frontalface_default.xml"
