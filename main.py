9978b6092cb474d7d635b19449bc111b42ba64d5
NathanielMention <nathanielmention@gmail.com>
2023-02-20 21:25:55 -0500

    setup camera frame/faces

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ import cv2
 # import xml files for faces from cv2
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute(
 ) / "data/haarcascade_frontalface_default.xml"
+
+# build classifier based on cascade_path file, finding faces in img data
+clf = cv2.CascadeClassifier(str(cascade_path))
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    # setup camera frame
+    _, frame = camera.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
